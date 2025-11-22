f39b4ae5323117db9ff13803b6aa296ccacd28f2
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-22 17:13:06 +0000

    m

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A"""
+
+
+def __init__(self, size=0):
+    """C"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
+    def area(self):
+        return self._size ** 2
