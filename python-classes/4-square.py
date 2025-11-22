10476614f83877b855368e6a78825ab9b193c773
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-22 17:35:16 +0000

    m

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,20 +1,32 @@
-#!/usr/bin/python3
-"""Module tha method."""
-
-
 class Square:
-    """Represation."""
+    """Represents a square with privn."""
 
     def __init__(self, size=0):
-        """In size."""
-        self.size = size  # setter will validate
+        """Initialize the square with optional size."""
+        self.size = size
 
     @property
     def size(self):
-        """Rere."""
+        """Retrieve the size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """idation."""
+        """Set the size of the square with validation."""
         if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print the square with the character #."""
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("#" * self.__size)
