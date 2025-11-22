ab6b5bf443304fd9cd3e7c577821454aff843ccd
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-22 17:22:23 +0000

    m

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """asa"""
 
+
 class Square:
     """ss"""
+
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
