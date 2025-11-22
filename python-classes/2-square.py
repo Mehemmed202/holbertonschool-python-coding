584bff2e10ae4a80a1b3a4c9599afdde66605b84
Mehemmed202 <m3mm3dov857@gmail.com>
2025-11-22 17:17:14 +0000

    m

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """asa"""
 
+class Square:
+    def __init__(self, size=0):
+        """ss"""
 
-def __init__(self, size=0):
-    """ss"""
-
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
-    def area(self):
-        return self._size ** 2
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        def area(self):
+            return self._size ** 2
